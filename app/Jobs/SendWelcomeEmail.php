67f6abbfd1bdafa9cf334793dede5061c0767abb
Kishore <kishore.m@swipewire.com>
2025-09-27 21:51:49 +0530

    Initial commit

---
+++ app/Jobs/SendWelcomeEmail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendWelcomeEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+
+        Mail::raw("Welcome {$this->user->name}!", function($message) {
+            $message->to($this->user->email)
+                    ->subject("Welcome to Our App");
+        });
+
+
+    }
+}
